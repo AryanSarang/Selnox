63c05c2c9ada8aec8681078aa5c299a4c2225918
Aryan Sarang <thearyansarang@gmail.com>
2023-09-14 18:38:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import 'react-quill/dist/quill.snow.css';
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-dropdown-tree-select/dist/styles.css';
+import { Routes, Route } from "react-router-dom";
+import RegistrationForm from './Components/RegistrationForm';
+import EmployeeList from './Components/EmployeeList';
+import SearchEmployee from './Components/SearchEmployee';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<RegistrationForm />} />
+        <Route path='/employeelist' element={<EmployeeList />} />
+        <Route path='/searchemployee' element={<SearchEmployee />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/SearchEmployee.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react"
+import DropdownTreeSelect from "react-dropdown-tree-select";
+import DatePicker from "react-datepicker";
+
+
+const SearchEmployee = (props) => {
+    const [dob, setDob] = useState(new Date());
+
+
+    const data = [{
+        label: "All employee",
+        value: "Allemployee",
+        expanded: "true",
+        children:
+            [
+                {
+                    label: 'search me 1',
+                    value: 'searchme',
+                    expanded: "true",
+                },
+                {
+                    label: 'All practitioner',
+                    value: 'Allpractitioner',
+                    expanded: "true",
+                    children: [
+                        {
+                            label: 'search me too',
+                            value: 'searchmetoo',
+
+                        },
+                        {
+                            label: 'search me ree',
+                            value: 'searchmetoo',
+
+                        },
+                    ],
+                }, {
+                    label: 'All Assistants',
+                    value: 'AllAssistants',
+                    expanded: "true",
+                    children: [
+                        {
+                            label: 'search me fr',
+                            value: 'searchmetoo',
+
+                        },
+                        {
+                            label: 'search me plz',
+                            value: 'searchmetoo',
+
+                        },
+                    ],
+                }]
+    }]
+
+
+    const onChange = (currentNode, selectedNodes, value) => {
+        console.log('onChange::', currentNode, selectedNodes);
+
+
+    }
+    const onAction = (node, action) => {
+        console.log('onAction::', action, node);
+
+    }
+    const onNodeToggle = currentNode => {
+        console.log('onNodeToggle::', currentNode)
+    }
+
+
+
+    return (
+        <div className="d-flex flex-md-row flex-sm-column empSearchDiv gap-5">
+
+            <div className="empDropDiv m-5 col-md-5 ">
+                <label for="drp-tree" class="form-label col-md-12 mb-3 headingLabel fs-3">Select Employee Dropdown</label>
+                <DropdownTreeSelect id="drp-tree" data={data} className="empDropdown"
+                    inlineSearchInput
+                    texts={{ placeholder: "Select employee", inlineSearchPlaceholder: "Search employee..." }}
+                    onChange={onChange} on onAction={onAction} onNodeToggle={onNodeToggle} />
+            </div>
+            <div className=" col-md-5  align-self-center">
+                <label for="basic-url3" class="form-label col-md-12 mb-3 headingLabel fs-3">Date Picker</label>
+                <DatePicker className="form-control dateSearch col-md-12 w-100"
+                    showIcon id="basic-url3"
+                    scrollableYearDropdow
+                    placeholderText="Pick Date"
+                    dateFormat="d-M-yy"
+                    selected={dob}
+                    onChange={(date) => setDob(date)} />
+            </div>
+        </div>
+    )
+};
+
+export default SearchEmployee;
